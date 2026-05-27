dd9a7ba0edc80693896b1e832816ab2d6c2bfb68
Davyd Edivaldo Silva <davydedivaldo@gmail.com>
2026-05-26 23:30:27 -0300

    .
    finalizando home page spider man

--- script.js
+++ script.js
@@ -1,17 +1,76 @@
 const point = document.querySelectorAll('.point');
 const slider = document.querySelectorAll('.img-slider');
 
-point.forEach((item, index)=>{
-    item.addEventListener('click', ()=>{
-        slider.forEach((img)=>{
-            img.style.opacity = "0";
-            
-        });
-        slider[index].style.opacity="1";
+let contSlider = 0;
 
-        point.forEach((e)=>{
+point.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        slider.forEach((img) => {
+            img.style.opacity = "0";
+
+        });
+        slider[index].style.opacity = "1";
+
+        point.forEach((e) => {
             e.classList.remove('active-point');
-        }) 
+        })
         point[index].classList.add('active-point');
     })
 })
+
+function slideShow(index) {
+    slider.forEach((img) => {
+        img.style.opacity = "0";
+
+    });
+    slider[index].style.opacity = "1";
+
+    point.forEach((e) => {
+        e.classList.remove('active-point');
+    })
+    point[index].classList.add('active-point');
+}
+
+setInterval(()=>{
+    contSlider++;
+    if(contSlider >= slider.length){
+        contSlider = 0;
+    }
+    slideShow(contSlider);
+},3000);
+
+
+//animação com gsap
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    var tl1 = gsap.timeline();
+
+    tl1
+    .to('.screen-loading',{
+        duration:2,
+        opacity:0,
+        delay:3,
+        ease: "power4.out",
+    })
+    .from('.bg-hero', {
+        duration: 1,
+        opacity: 0,
+        stagger:{each: 0.1},
+        ease: 'power4.out',  
+    }, '-=1.0')
+    .from('nav div',{
+        duration: 2,
+        opacity: 0,
+        y:400,
+        stagger:{each:0.2},
+        ease: 'back.out(1.7)',
+    }, '-=0.5')
+    .from('.box-hero div',{
+        duration: 2,
+        opacity: 0,
+        y:200,
+        stagger:{each:0.2},
+        ease: 'back.out(1.7)',
+    },'-=1.7')
+
+});
