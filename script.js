2c4b36861fcfdfa997aa530a0f3c5d8fe2bbfd12
davyd <davydedivaldo@gmail.com>
2026-05-29 00:11:20 -0300

    .

--- script.js
+++ script.js
@@ -42,35 +42,35 @@ setInterval(()=>{
 
 //animação com gsap
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    var tl1 = gsap.timeline();
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     var tl1 = gsap.timeline();
 
-    tl1
-    .to('.screen-loading',{
-        duration:2,
-        opacity:0,
-        delay:3,
-        ease: "power4.out",
-    })
-    .from('.bg-hero', {
-        duration: 1,
-        opacity: 0,
-        stagger:{each: 0.1},
-        ease: 'power4.out',  
-    }, '-=1.0')
-    .from('nav div',{
-        duration: 2,
-        opacity: 0,
-        y:400,
-        stagger:{each:0.2},
-        ease: 'back.out(1.7)',
-    }, '-=0.5')
-    .from('.box-hero div',{
-        duration: 2,
-        opacity: 0,
-        y:200,
-        stagger:{each:0.2},
-        ease: 'back.out(1.7)',
-    },'-=1.7')
+//     tl1
+//     .to('.screen-loading',{
+//         duration:2,
+//         opacity:0,
+//         delay:3,
+//         ease: "power4.out",
+//     })
+//     .from('.bg-slider', {
+//         duration: 1,
+//         opacity: 0,
+//         stagger:{each: 0.1},
+//         ease: 'power4.out',  
+//     }, '-=1.0')
+//     .from('nav div',{
+//         duration: 2,
+//         opacity: 0,
+//         y:400,
+//         stagger:{each:0.2},
+//         ease: 'back.out(1.7)',
+//     }, '-=0.5')
+//     .from('.box-hero div',{
+//         duration: 2,
+//         opacity: 0,
+//         y:200,
+//         stagger:{each:0.2},
+//         ease: 'back.out(1.7)',
+//     },'-=1.7')
 
-});
+// });
